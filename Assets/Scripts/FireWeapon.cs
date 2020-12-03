1a2c1fad1ccd0bbe3531a6ab25965351afa86261
DESKTOP-J9HR1D8\Kevin <vulthuryol1111@gmail.com>
2020-12-03 13:03:05 -0500

    Making a 1911
    Added assets for firearms models, visual effects, and audio

---
+++ Assets/Scripts/FireWeapon.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWeapon : MonoBehaviour
+{
+    public int damage;
+
+    public int magazineCapacity;
+
+    public float rateOfFire;
+
+    public float projectileSpread;
+
+    public float range;
+
+    public float reloadTime;
+
+    public bool isFullAuto;
+
+    int shotsFired;
+
+    public int shotsRemaining;
+
+    public bool shooting;
+
+    public bool reloading;
+
+    public bool triggerSet;
+
+    public Camera fpsCam;
+
+    public Transform muzzle;
+
+    public RaycastHit rayHit;
+
+    public LayerMask whatIsEnemy;
+
+    public GameObject projectileImpactGraphic;
+    public ParticleSystem muzzleFlash;
+
+    public AudioSource sound;
+
+
+    private void Awake(){
+        shotsRemaining = magazineCapacity;
+        triggerSet = true;
+    }
+    private void WeaponInputs()
+    {
+        shooting =
+            isFullAuto
+                ? Input.GetKey(KeyCode.Mouse0)
+                : Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (
+            Input.GetKeyDown(KeyCode.R) &&
+            shotsRemaining < magazineCapacity &&
+            !reloading || shotsRemaining == 0
+        )
+        {
+            Reload();
+        }
+
+        if (triggerSet && shooting && !reloading && shotsRemaining > 0)
+        {
+            Shoot();
+        }
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("Reloaded", reloadTime);
+    }
+
+    private void Reloaded()
+    {
+        reloading = false;
+        shotsRemaining = magazineCapacity;
+    }
+
+    private void Shoot()
+    {
+        // Instantiate(muzzleFlash, muzzle.position, muzzle.rotation);
+        muzzleFlash.Play();
+        sound.Play();
+
+        shotsRemaining--;
+        triggerSet = false;
+
+        float x = Random.Range(-projectileSpread, projectileSpread);
+        float y = Random.Range(-projectileSpread, projectileSpread);
+
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
+
+        if (
+            Physics
+                .Raycast(fpsCam.transform.position,
+                fpsCam.transform.forward,
+                out rayHit,
+                range,
+                whatIsEnemy)
+        )
+        {
+            Instantiate(projectileImpactGraphic, rayHit.point, Quaternion.LookRotation(rayHit.normal));
+            Debug.Log(rayHit.collider.name);
+
+            // if (rayHit.collider.CompareTag("Enemy"))
+            // {
+                // rayHit.collider.GetComponent<EnemyAI>().TakeDamage(damage);
+            // }
+        }
+
+        Invoke("TriggerReset", 60f / rateOfFire);
+    }
+
+    private void TriggerReset()
+    {
+        triggerSet = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WeaponInputs();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float playerSpeed = 10f;
+    private float playerJumpHeight = 4f;
+    private float gravitationalForce = -20f;
+    private CharacterController controller;
+
+    Vector3 playerVelocity;
+    Vector3 graviationalVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movementInput = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+
+        if (controller.isGrounded)
+        {
+            graviationalVelocity.y = gravitationalForce * Time.deltaTime;
+            if(Input.GetButtonDown("Jump")){
+                graviationalVelocity.y += Mathf.Sqrt(playerJumpHeight * -2f * gravitationalForce);
+            }
+
+            if(Input.GetKey(KeyCode.LeftShift) || controller.height == 2 && Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out RaycastHit hit, 2.5f)){
+                playerVelocity = movementInput * playerSpeed * 0.5f;
+                controller.height = 2f;
+            }
+            else{
+                playerVelocity = movementInput * playerSpeed;
+                controller.height = 3.8f;
+            }
+            
+        }
+        else {
+            graviationalVelocity.y += gravitationalForce * Time.deltaTime;
+        }
+        controller.Move((playerVelocity + graviationalVelocity) * Time.deltaTime);
+    }
+}
