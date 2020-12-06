24a57e5df87cae05eab9ace18f7733a96a7777c5
Eliezer Encarnacion <35782951+eliel15000@users.noreply.github.com>
2020-12-05 20:31:55 -0500

    Update WeaponContainer.cs

--- Assets/Scripts/WeaponContainer.cs
+++ Assets/Scripts/WeaponContainer.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class WeaponContainer : MonoBehaviour
 {
+    // Weapon List
+    public GameObject primaryWeapon;
+    public GameObject secondaryWeapon;
+
+    // Which weapon is equipped
+    private bool isPrimaryEquipped;
+    private bool switching;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        primaryWeapon.SetActive(true);
+        secondaryWeapon.SetActive(false);
+        isPrimaryEquipped = true;
+        switching = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Tab) && !switching)
+        {
+            switching = true;
+            if (isPrimaryEquipped)
+            {
+                primaryWeapon.SetActive(false);
+                Invoke("switchWeapon", 1.0f);
+                // secondaryWeapon.SetActive(true);
+                // isPrimaryEquipped = false;
+            }
+            else
+            {
+                secondaryWeapon.SetActive(false);
+                Invoke("switchWeapon", 1.0f);
+                // primaryWeapon.SetActive(true);
+                // isPrimaryEquipped = true;
+            }
+        }
+    }
+
+    private void switchWeapon()
+    {
+        if (isPrimaryEquipped)
+        {
+            secondaryWeapon.SetActive(true);
+            isPrimaryEquipped = false;
+            switching = false;
+        }
+        else
+        {
+            primaryWeapon.SetActive(true);
+            isPrimaryEquipped = true;
+            switching = false;
+        }
     }
 }
