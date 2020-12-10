4a94de7f0978de3d64b8035e6f27f7d5e8d92ecd
knsanchez0001 <knsanchez@umass.edu>
2020-12-09 21:17:29 -0500

    Weapon ammo can be replenished with ammo boxes

--- Assets/Scripts/Regenerate.cs
+++ Assets/Scripts/Regenerate.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Regenerate : MonoBehaviour
 {
+    public int type;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnTriggerEnter(Collider collision)
@@ -21,12 +21,60 @@ public class Regenerate : MonoBehaviour
         GameObject other = collision.gameObject;
         if (collision.gameObject.layer == 8)
         {
-            Health health = other.GetComponent<Health>();
-            if(health.health < health.maxHealth){
-                health.Heal();
-                Destroy(gameObject);
+            Debug.Log(gameObject.name);
+            if (type == 0)
+            {
+                Health health = other.GetComponent<Health>();
+                if (health.health < health.maxHealth)
+                {
+                    health.Heal();
+                    Destroy (gameObject);
+                }
             }
-            
+
+            // else
+            // {
+            //     Weapon w;
+            //     if(type == 1)
+            //     {
+            //         w = GameObject.FindWithTag("Pistol").GetComponent<Weapon>();
+            //     }
+            //     else if (type == 2)
+            //     {
+            //         w = GameObject.FindWithTag("Rifle").GetComponent<Weapon>();
+            //     }
+            //     if(w.totalShotsRemaining < w.totalCapacity)
+            //     {
+            //         w.totalShotsRemaining = w.totalCapacity;
+            //         Destroy (gameObject);
+            //     }
+            // }
+            if (type == 1)
+            {
+                GameObject pistol = GameObject.FindWithTag("Pistol");
+                if (pistol != null)
+                {
+                    Weapon weapon = pistol.GetComponent<Weapon>();
+                    if (weapon.totalShotsRemaining < weapon.totalCapacity)
+                    {
+                        weapon.totalShotsRemaining = weapon.totalCapacity;
+                        Destroy (gameObject);
+                    }
+                }
+            }
+            if (type == 2)
+            {
+                GameObject rifle = GameObject.FindWithTag("Rifle");
+                if (rifle != null)
+                {
+                    Weapon weapon = rifle.GetComponent<Weapon>();
+                    if (weapon.totalShotsRemaining < weapon.totalCapacity)
+                    {
+                        weapon.totalShotsRemaining = weapon.totalCapacity;
+                        Destroy (gameObject);
+                    }
+                }
+            }
         }
     }
 }
