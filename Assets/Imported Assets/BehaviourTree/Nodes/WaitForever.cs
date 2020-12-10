6347e78cde3eccb35fe917bc350001f5d1516091
knsanchez0001 <knsanchez@umass.edu>
2020-12-10 00:01:26 -0500

    Removed unused imports

--- Assets/Imported Assets/BehaviourTree/Nodes/WaitForever.cs
+++
@@ -1,8 +0,0 @@
-﻿/// <summary>
-/// Pause forever
-/// </summary>
-/// <typeparam name="X">The agent type</typeparam>
-public class WaitForever<X> : WaitNode<X> {
-
-    public WaitForever() : base(100000000000) { }
-}
